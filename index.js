3f6cbad7f209e31b18112ffe3c10652a05b54059
lionheart2200 <61442080+lionheart2200@users.noreply.github.com>
2024-08-30 06:47:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 
 
-const DBC = "mongodb+srv://lionheart2200:AJW1PVZ13bMjzz0u@cluster0.fwebi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DBC = "mongodb+srv://meshchemistry:<Z9Woj4TNkAE17hky>@cluster0.govhh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const PORT = 3020
 
